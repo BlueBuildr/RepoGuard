60c17b7eb85895c21b66319d9799536d1254194a
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2026-03-09 18:44:08 +0100

    wip

---
+++ RepoGuard.ps1
@@ -0,0 +1,28 @@
+param (
+
+)
+
+# Pull request related functions
+function Get-PullRequest {}
+
+function Get-PullRequestFiles {}
+
+function Assert-PullRequestState {}
+
+function Test-PullRequestAllowedFiles {}
+
+# Release related functions
+function Get-Release {}
+
+function New-Release {}
+
+function Get-ReleaseTag {}
+
+function New-ReleaseNotes {}
+
+function Update-ReleaseNotes {}
+
+# GitHub API related functions
+function Invoke-GitHubRestMethod {}
+
+
